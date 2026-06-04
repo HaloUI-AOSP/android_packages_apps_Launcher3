048249e064d7bc159b8b4da256163f9da1a479b5
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-04 18:23:01 +0000

    Launcher3: Debug overview logs
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- quickstep/src/com/android/quickstep/views/OverviewActionsView.java
+++ quickstep/src/com/android/quickstep/views/OverviewActionsView.java
@@ -54,6 +54,7 @@ import java.util.Arrays;
 public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayout
         implements OnClickListener, Insettable {
     public static final String TAG = "OverviewActionsView";
+    private static final boolean DEBUG = false;
     private final Rect mInsets = new Rect();
 
     /**
@@ -289,8 +290,10 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
      *                      pair.
      */
     public void updateForGroupedTask(boolean isGroupedTask, boolean canSaveAppPair) {
-        Log.d(TAG, "updateForGroupedTask() called with: isGroupedTask = [" + isGroupedTask
-                + "], canSaveAppPair = [" + canSaveAppPair + "]");
+        if (DEBUG) {
+            Log.d(TAG, "updateForGroupedTask() called with: isGroupedTask = [" + isGroupedTask
+                    + "], canSaveAppPair = [" + canSaveAppPair + "]");
+        }
         mIsGroupedTask = isGroupedTask;
         mCanSaveAppPair = canSaveAppPair;
         updateActionButtonsVisibility();
@@ -316,8 +319,10 @@ public class OverviewActionsView<T extends OverlayUICallbacks> extends FrameLayo
         boolean showGroupActions = mIsGroupedTask && mDp.getDeviceProperties().isLargeScreen()
                 && mCanSaveAppPair
                 && !getContext().getSystemService(ActivityManager.class).isLowRamDevice();
-        Log.d(TAG, "updateActionButtonsVisibility() called: showSingleTaskActions = ["
-                + showSingleTaskActions + "], showGroupActions = [" + showGroupActions + "]");
+        if (DEBUG) {
+            Log.d(TAG, "updateActionButtonsVisibility() called: showSingleTaskActions = ["
+                    + showSingleTaskActions + "], showGroupActions = [" + showGroupActions + "]");
+        }
         getActionsAlphas().get(INDEX_GROUPED_ALPHA).setValue(showSingleTaskActions ? 1 : 0);
         getGroupActionsAlphas().get(INDEX_GROUPED_ALPHA).setValue(showGroupActions ? 1 : 0);
     }
